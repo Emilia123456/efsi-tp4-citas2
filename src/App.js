ead3339b086f1532007d68834d6a470c03f4caf1
Emilia Armeno <114943230+Emilia123456@users.noreply.github.com>
2024-05-14 08:32:28 -0300

    Add files via upload
    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import { Cita } from './components/Cita/Cita';
+import { Formulario } from './components/Formulario/Formulario';
+import React, {useState} from 'react';
+
+function App() {
+  return (<>
+        <h1>ADMINISTRADOR DE PACIENTES</h1>
+        <div class="container">
+          <div class="row">
+            <div class="one-half column">
+              <h2>Crear mi Cita</h2>
+              <Formulario  />
+            </div>
+            <div className="one-half column">
+              <Cita mascota="Pipa" dueño="Martin" fecha="2021-08-05" hora="08:20" sintomas="Le duele la pierna" />
+              <Cita mascota="Sifon" dueño="Flecha" fecha="2023-08-05" hora="09:24" sintomas="Duerme mucho" />
+              <Cita mascota="Floki" dueño="Ari" fecha="2023-08-05" hora="16:15" sintomas="No está comiendo" />
+            </div>
+          </div>
+        </div>
+      </>
+  );
+}
+
+const [citas, setCitas] = useState([Cita]);
+
+export default App;
